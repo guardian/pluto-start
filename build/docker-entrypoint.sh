81c9aa83a36b8e37ef2ea8ae80525f19d55ced4b
Andy Gallagher <andy.gallagher@theguardian.com>
2020-06-24 12:08:07 +0100

    fix non-working auth url build and nginx not outputting the oauth config

---
+++ build/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d /etc/oauth-config ]; then
+  echo ERROR: Can\'t find /etc/oauth-config for accessing the oauth config
+  exit 1
+fi
+
+mkdir -p /usr/share/nginx/html/meta/oauth
+cp -avL /etc/oauth-config/* /usr/share/nginx/html/meta/oauth
+
+nginx -g 'daemon off;'
