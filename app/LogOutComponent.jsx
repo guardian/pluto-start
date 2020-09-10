c02d1be227e46a34cee3709a8012c9af3ea6f9a3
Dave Allison <david.allison@theguardian.com>
2020-09-10 13:53:10 +0100

    Change the text on the log out page and centre it

--- app/LogOutComponent.jsx
+++ app/LogOutComponent.jsx
@@ -6,8 +6,9 @@ class LogOutComponent extends React.Component {
   render() {
     return (
       <div className="logout">
-        <h1 className="logout_heading">Logging out</h1>
-        <Link to="/">Return to log in page.</Link>
+        <h1 className="logout_heading">Logged out</h1>
+        Log out process completed. Now you can either close this window or click
+        the "LOGIN" button above to log in again.
       </div>
     );
   }
